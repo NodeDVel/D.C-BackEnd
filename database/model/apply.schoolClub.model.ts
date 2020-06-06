2fdfefd214c46cd1bb02105cd4703f095f69b653
rlalasrl <rlaalsrl0810@naver.com>
2020-06-06 13:05:28 +0900

    creating apply.schoolClub Database

---
+++ database/model/apply.schoolClub.model.ts
@@ -0,0 +1,71 @@
+import {
+  AllowNull,
+  Column,
+  CreatedAt,
+  DataType,
+  Model,
+  PrimaryKey,
+  Table,
+  UpdatedAt,
+} from 'sequelize-typescript';
+
+@Table({
+  timestamps: true,
+})
+export default class Apply extends Model<Apply> {
+  @PrimaryKey
+  @AllowNull(false)
+  @Column(DataType.UUID)
+  public pk: string;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  public name: string;
+
+  @AllowNull(false)
+  @Column(DataType.INTEGER)
+  public classNum: number;
+
+  @AllowNull(false)
+  @Column(DataType.INTEGER)
+  public studentNum: number;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  public password: string;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  public passwordKey: string;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  public phoneNumber: string;
+
+  @AllowNull(false)
+  @Column(DataType.STRING)
+  public field: string;
+
+  @AllowNull(false)
+  @Column(DataType.TEXT)
+  public content: string;
+
+  @AllowNull(false)
+  @Column(DataType.TEXT)
+  public reason: string;
+
+  @AllowNull(false)
+  @Column(DataType.TEXT)
+  public language: string;
+
+  @AllowNull(false)
+  @Column(DataType.BOOLEAN)
+  public isSubmit: boolean;
+
+  @CreatedAt
+  public createdAt: Date;
+
+  @UpdatedAt
+  public updatedAt: Date;
+
+}
